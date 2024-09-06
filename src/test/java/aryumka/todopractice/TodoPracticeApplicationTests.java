366e6a3b2e79123ac72db1cf4f5b30fb1afca4c1
areum <areum@jobis.co>
2024-09-06 23:06:25 +0900

    Initial commit

---
+++ src/test/java/aryumka/todopractice/TodoPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package aryumka.todopractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
